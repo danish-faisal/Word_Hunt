4fecaabe5b96282dbe8f835cecdcfea0906e9e48
= <danishfaisal.wwe@gmail.com>
2022-02-16 21:59:05 +0530

    API call setup, basic bg styling & axios,@emotion/react,@emotion/styles dependencies added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { Container } from '@mui/material';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [meanings, setMeanings] = useState([]);
+  const [word, setWord] = useState("");
+
+  async function dictionaryAPI() {
+    try {
+      const data = await axios('https://api.dictionaryapi.dev/api/v2/entries/en/plane');
+
+      console.log(data.data);
+    } catch (error) {
+      console.error("Error:" + error);
+    }
+  }
+
+  useEffect(() => {
+    dictionaryAPI();
+  }, []);
+
   return (
-    <div className="App">
-      Hello World
+    <div className="App" style={{ height: '100vh', backgroundColor: '#282c34', color: '#fff' }}>
+      <Container maxWidth="md" style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+        Dictionary
+      </Container>
     </div>
   );
 }
