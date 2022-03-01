ecb2a4d54021b986c9c0a9ab10f96f9b9b329a1a
= <danishfaisal.wwe@gmail.com>
2022-03-01 21:35:52 +0530

    Definitions component complete

--- src/components/Definitions/Definitions.js
+++ src/components/Definitions/Definitions.js
@@ -1,37 +1,54 @@
-import React from 'react';
-import './Definitions.css'
+import React from "react";
+import "./Definitions.css";
 
-const Definitions = ({ word, meanings }) => {
+const Definitions = ({ meanings, word, LightTheme }) => {
+    const audioUrl = meanings && meanings[0] && meanings[0].phonetics[0] ? meanings[0].phonetics[0].audio : "";
+
     return (
-        <div className='meanings'>
-            {word === "" ?
-                <span className='subTitle'>Start by typing a word in search</span> :
-                (
-                    meanings.map((obj) => (
-                        obj.meanings.map((meaning) => (
-                            meaning.definitions.map((def) => (
-                                <div className='singleMeaning' style={{ backgroundColor: "white", color: "black" }}>
-                                    <b>{def.definition}</b>
-                                    <hr />
-                                    {
-                                        def.example && (<span><b>Example: </b>{def.example}</span>)
-                                    }
-                                    {
-                                        def.synonyms && (
-                                            <span>
-                                                <b>Synonyms: </b>
-                                                {def.synonyms.map((synonym) => `${synonym}, `)}
-                                            </span>
-                                        )
-                                    }
-                                </div>
-                            ))
+        <div className="meanings">
+            {/* audio---------------------------- */}
+            {meanings[0] && word && audioUrl && (
+                <audio className="audioComp"
+                    src={audioUrl}
+                    controls
+                >
+                    Your browser does not support the audio element.
+                </audio>
+            )}
+            {/* audio---------------------------- */}
+
+            {word === "" ? (
+                <span className="subTitle">Start by typing a word in search</span>
+            ) : (
+                meanings.map((obj) =>
+                    obj.meanings.map((meaning) =>
+                        meaning.definitions.map((def) => (
+                            <div
+                                className="singleMeaning"
+                                style={{
+                                    backgroundColor: LightTheme ? "#3b5360" : "white",
+                                    color: LightTheme ? "white" : "black",
+                                }}
+                            >
+                                {def.definition}
+                                <hr className="diff" />
+                                {def.example && (
+                                    <span>
+                                        <b>Example :</b> {def.example}
+                                    </span>
+                                )}
+                                {def.synonyms && def.synonyms.length > 0 && (
+                                    <span>
+                                        <b>Synonyms :</b> {def.synonyms.map((s) => `${s}, `)}
+                                    </span>
+                                )}
+                            </div>
                         ))
-                    ))
+                    )
                 )
-            }
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default Definitions;
